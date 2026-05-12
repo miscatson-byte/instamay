746c10b6aede7632f0f45a33f21df77dee3ba8c1
miscatson-byte <misca@gmail.com>
2026-05-12 23:19:32 +0530

    change home icon to react-icons

--- frontendClient/src/assets/icons.jsx
+++ frontendClient/src/assets/icons.jsx
@@ -1,3 +1,5 @@
+import { AiFillHome } from 'react-icons/ai';
+
 export function Chat() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
@@ -22,10 +24,8 @@ export function Bell(){
   )
 }
 
-export function Home() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-  )
+export function Home(){
+  return <AiFillHome size={30} color="#000" />
 }
 
 export function Add(){
